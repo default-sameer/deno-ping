3f6c0e8d9ca6592fd8f067092fae81f14f49e84b
thesameerjoshi <sameer.joshi@smaitic.com>
2025-01-31 13:27:51 +0545

    query param in API

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("id");
   return NextResponse.json(
-    { message: "Handshake successful" },
+    {
+      message: query
+        ? `Handshake successful with ${query}`
+        : "Handshake successful",
+    },
     { status: 200 }
   );
 }
